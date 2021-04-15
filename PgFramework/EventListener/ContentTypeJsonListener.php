a3f9c18e7d0f50843ceb77f48c5b9ccf5ae42bff
William Lety <william.lety@gmail.com>
2021-04-15 15:02:14 +0200

    Initial commit

--- PgFramework/EventListener/ContentTypeJsonListener.php
+++ PgFramework/EventListener/ContentTypeJsonListener.php
@@ -12,7 +12,11 @@ class ContentTypeJsonListener
         $request = $event->getRequest();
         if (RequestUtils::isJson($request)) {
             $response = $event->getResponse();
-            $event->setResponse($response->withAddedHeader('Content-type', 'application/json;charset=UTF-8'));
+            $event->setResponse(
+                $response->withAddedHeader('Content-type', 'application/json;charset=UTF-8')
+                    ->withAddedHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+                    ->withAddedHeader('Cross-Origin-Opener-Policy', 'cross-origin')
+            );
         }
     }
 }
