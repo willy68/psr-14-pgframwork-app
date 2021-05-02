c2133f515a2439e8e840e0bc55c007ad4a6adf2a
willy68 <william.lety@gmail.com>
2021-05-02 18:29:28 +0200

    Initial commit

---
+++ PgFramework/Response/JsonResponse.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PgFramework\Response;
+
+use GuzzleHttp\Psr7\Response;
+
+class JsonResponse extends Response
+{
+    public function __construct(
+        $status = 200,
+        $body = null,
+        $reason = null)
+    {
+        parent::__construct($status, ['content-type', 'application/json;charset=UTF-8'], $body, '1.1', $reason);
+    }
+}
