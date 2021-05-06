f72e5c9abaef6ccc9aa72cae4d6351ae96e4c107
William Lety <william.lety@gmail.com>
2021-05-06 11:24:59 +0200

    Initial commit

---
+++ PgFramework/Security/Hasher/DefaultPasswordHasher.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PgFramework\Security\Hasher;
+
+class DefaultPasswordHasher implements PasswordHasherInterface
+{
+    protected $config = [
+        'algo' => \PASSWORD_BCRYPT,
+        'options' => [
+            'cost' => 10
+        ]
+    ];
+
+    public function __construct(array $config = [])
+    {
+        if (!empty($config)) {
+            $this->config = array_merge($this->config, $config);
+        }
+        if (!in_array($this->config['algo'], \password_algos())) {
+            $this->config['algo'] = \PASSWORD_DEFAULT;
+        }
+    }
+
+    public function hash(string $plainPassword)
+    {
+        return \password_hash($plainPassword, $this->config['algo'], $this->config['options']);
+    }
+
+    public function verify(string $hashedPassword, string $plainPassword): bool
+    {
+        return \password_verify($plainPassword, $hashedPassword);
+    }
+
+    public function needsRehash(string $hashedPassword): bool
+    {
+        return \password_needs_rehash(
+            $hashedPassword,
+            $this->config['algo'],
+            $this->config['options']
+        );
+    }
+}
---
+++ PgFramework/Security/Hasher/PepperPasswordHasher.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PgFramework\Security\Hasher;
+
+use PgFramework\Security\Security;
+
+class PepperPasswordHasher implements PasswordHasherInterface
+{
+    protected $config = [
+        'algo' => \PASSWORD_BCRYPT,
+        'options' => [
+            'cost' => 10,
+            'memory_cost' => \PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'time_cost' => \PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'threads' => PASSWORD_ARGON2_DEFAULT_THREADS
+        ]
+    ];
+
+    public function __construct(array $config = [])
+    {
+        if (!empty($config)) {
+            $this->config = array_merge($this->config, $config);
+        }
+
+        if (!in_array($this->config['algo'], \password_algos())) {
+            $this->config['algo'] = \PASSWORD_DEFAULT;
+        }
+    }
+
+    public function hash(string $plainPassword)
+    {
+        $plainPassword = \hash_hmac("sha256", $plainPassword, Security::getSalt());
+        return \password_hash($plainPassword, $this->config['algo'], $this->config['options']);
+    }
+
+    public function verify(string $hashedPassword, string $plainPassword): bool
+    {
+        $plainPassword = \hash_hmac("sha256", $plainPassword, Security::getSalt());
+        return \password_verify($plainPassword, $hashedPassword);
+    }
+
+    public function needsRehash(string $hashedPassword): bool
+    {
+        return \password_needs_rehash(
+            $hashedPassword,
+            $this->config['algo'],
+            $this->config['options']
+        );
+    }
+}
