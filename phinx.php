1032215afbdc9f8739954866d765a601b614011d
willy68 <william.lety@gmail.com>
2022-02-27 18:00:21 +0100

    Initial commit

--- phinx.php
+++ phinx.php
@@ -1,34 +1,35 @@
 <?php
+
 require 'app/Bootstrap/Bootstrap.php';
 
 $migrations = [];
 $seeds = [];
 foreach ($app->getModules() as $module) {
-  if ($module::MIGRATIONS) {
-      $migrations[] = $module::MIGRATIONS;
-  }
-  if ($module::SEEDS) {
-    $seeds[] = $module::SEEDS;
-  }
+    if ($module::MIGRATIONS) {
+        $migrations[] = $module::MIGRATIONS;
+    }
+    if ($module::SEEDS) {
+        $seeds[] = $module::SEEDS;
+    }
 }
 
 return [
-  'paths' => [
-    'migrations' => $migrations,
-    'seeds' => $seeds
-  ],
-  'environments' => [
-    'default_database' => 'development',
-    
-    'development' => [
-      'adapter' => 'mysql',
-      'host' => $app->getContainer()->get('database.host'),
-      'name' => $app->getContainer()->get('database.name'),
-      'user' => $app->getContainer()->get('database.user'),
-      'pass' => $app->getContainer()->get('database.password'),
-      'charset' => 'utf8'
+    'paths' => [
+        'migrations' => $migrations,
+        'seeds' => $seeds
+    ],
+    'environments' => [
+        'default_database' => 'development',
+
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => $app->getContainer()->get('database.host'),
+            'name' => $app->getContainer()->get('database.name'),
+            'user' => $app->getContainer()->get('database.user'),
+            'pass' => $app->getContainer()->get('database.password'),
+            'charset' => 'utf8'
+        ]
     ]
-  ]
 ];
 
 /*
