8dc30f19f2a4f738f7bc4d081f3d373e4f1724e4
William Lety <william.lety@gmail.com>
2022-03-24 11:06:08 +0100

    Doctrine: Migrations Config

--- cli-config.php
+++ cli-config.php
@@ -2,15 +2,16 @@
 
 require 'app/Bootstrap/Bootstrap.php';
 
-use Doctrine\ORM\EntityManager;
 use Doctrine\Migrations\DependencyFactory;
 use Doctrine\Migrations\Configuration\Migration\PhpFile;
-use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\EntityManager\ManagerRegistryEntityManager;
+use Doctrine\Persistence\ManagerRegistry;
 
 $migrationsConfig = new PhpFile('migrations.php');
 
-$entityManager = $app->getContainer()->get(EntityManager::class);
+$Manager = $app->getContainer()->get(ManagerRegistry::class);
 
-$depencyFactory = DependencyFactory::fromEntityManager($migrationsConfig, new ExistingEntityManager($entityManager));
-
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
+return DependencyFactory::fromEntityManager(
+    $migrationsConfig,
+    ManagerRegistryEntityManager::withSimpleDefault($Manager, 'default')
+);
