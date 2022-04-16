68c0315f591c9cd806ea77ece85e5243bed88505
William Lety <william.lety@gmail.com>
2022-04-16 09:00:52 +0200

    Simplify Dispatcher

--- PgFramework/Middleware/DispatcherMiddleware.php
+++ PgFramework/Middleware/DispatcherMiddleware.php
@@ -4,22 +4,17 @@ declare(strict_types=1);
 
 namespace PgFramework\Middleware;
 
-use GuzzleHttp\Psr7\Response;
-use Invoker\CallableResolver;
+use Mezzio\Router\Route;
+use Mezzio\Router\RouteGroup;
 use Mezzio\Router\RouteResult;
 use Mezzio\Router\FastRouteRouter;
 use Mezzio\Router\RouterInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
-use Invoker\Reflection\CallableReflection;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Invoker\ParameterResolver\ParameterResolver;
 
-/**
- * Toute la magie est là
- */
 class DispatcherMiddleware implements MiddlewareInterface
 {
     /**
@@ -37,8 +32,6 @@ class DispatcherMiddleware implements MiddlewareInterface
     private $router;
 
     /**
-     *
-     *
      * @param ContainerInterface $container
      * @param Invoker $invoker
      */
@@ -68,7 +61,7 @@ class DispatcherMiddleware implements MiddlewareInterface
         }
 
         $this->router = $this->container->get(RouterInterface::class);
-        $this->prepareMiddlewareStack($this->router, $result);
+        $this->prepareMiddlewareStack($result);
 
         return (new CombinedMiddleware($this->container, $this->router->getMiddlewareStack(), $next))
             ->process($request, $next);
@@ -77,111 +70,29 @@ class DispatcherMiddleware implements MiddlewareInterface
     /**
      * Prepare la pile de middlewares du router
      *
-     * @param Router $router
-     * @param Route|null $route
+     * @param RouteResult $result
      * @return void
      */
-    protected function prepareMiddlewareStack(FastRouteRouter $router, ?RouteResult $result): void
+    protected function prepareMiddlewareStack(RouteResult $result): void
     {
+        /** @var Route $route */
         if ($route = $result->getMatchedRoute()) {
-            /** router stack first */
+            // router stack first
             if ($this->container->has('router.middlewares')) {
-                $router->middlewares($this->container->get('router.middlewares'));
+                $this->router->middlewares($this->container->get('router.middlewares'));
             }
-            /** @var Route $route */
+            /** @var RouteGroup $group */
             if ($group = $route->getParentGroup()) {
                 foreach ($group->getMiddlewareStack() as $middleware) {
-                    $router->middleware($middleware);
+                    $this->router->middleware($middleware);
                 }
             }
-            /**$route stack */
+            // $route stack
             foreach ($route->getMiddlewareStack() as $middleware) {
-                $router->middleware($middleware);
+                $this->router->middleware($middleware);
             }
-            /** wrap$route callable end */
-            $router->middleware($this->routeCallableMiddleware($result, $this->container));
+            // wrap $route callable end
+            $this->router->middleware(new RouteCallerMiddleware($result, $this->container));
         }
-    }
-
-    /**
-     * Wrap $route callable controller in middleware
-     *
-     * @param RouteResult $route
-     * @param ContainerInterface $container
-     * @return MiddlewareInterface
-     */
-    protected function routeCallableMiddleware(
-        RouteResult $result,
-        ContainerInterface $container
-    ): MiddlewareInterface {
-        return new class ($result, $container) implements MiddlewareInterface
-        {
-            /**
-             * Route result
-             *
-             * @var RouteResult
-             */
-            protected $result;
-
-            /**
-             * ContainerInterface
-             *
-             * @var ContainerInterface
-             */
-            protected $container;
-
-            /**
-             *  constructor.
-             * @param RouteResult $result
-             * @param ContainerInterface $container
-             */
-            public function __construct(RouteResult $result, ContainerInterface $container)
-            {
-                $this->result = $result;
-                $this->container = $container;
-            }
-
-            /**
-             * Psr15 middleware process method
-             * @param ServerRequestInterface $request
-             * @param RequestHandlerInterface $requestHandler
-             * @return ResponseInterface
-             */
-            public function process(
-                ServerRequestInterface $request,
-                RequestHandlerInterface $requestHandler
-            ): ResponseInterface {
-
-                $callback = $this->result->getMatchedRoute()->getCallback();
-                $params = $this->result->getMatchedParams();
-
-                if ($this->container instanceof \DI\Container) {
-                    $this->container->set(ServerRequestInterface::class, $request);
-                } else {
-                    // Limitation: $request must be named "$request"
-                    $params = array_merge(["request" => $request], $params);
-                }
-
-                /** @var CallableResolver */
-                $callableResolver = $this->container->get(CallableResolver::class);
-                $callback = $callableResolver->resolve($callback);
-                $callableReflection = CallableReflection::create($callback);
-
-                /** @var ParameterResolver $paramResolver */
-                $paramResolver = $this->container->get(ParameterResolver::class);
-                $params = $paramResolver->getParameters($callableReflection, $params, []);
-
-                $response = $callback(...$params);
-
-                if (is_string($response)) {
-                    return new Response(200, [], $response);
-                } elseif ($response instanceof ResponseInterface) {
-                    return $response;
-                } else {
-                    throw new \Exception('The response is not a string or a ResponseInterface');
-                }
-                return $requestHandler->handle($request);
-            }
-        };
     }
 }
