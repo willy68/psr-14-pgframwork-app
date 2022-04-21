ea3ae6dea110756407fedf30c2a65732097b7519
William Lety <william.lety@gmail.com>
2022-04-21 10:07:17 +0200

    Add ResponseSender

---
+++ PgFramework/Response/ResponseSender.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PgFramework\Response;
+
+use Psr\Http\Message\ResponseInterface;
+
+class ResponseSender
+{
+    /**
+     * Send an HTTP response
+     *
+     * @return void
+     */
+    public static function send(ResponseInterface $response)
+    {
+        // headers have already been sent by the developer
+        if (! headers_sent()) {
+            $http_line = sprintf(
+                'HTTP/%s %s %s',
+                $response->getProtocolVersion(),
+                $response->getStatusCode(),
+                $response->getReasonPhrase()
+            );
+    
+            header($http_line, true, $response->getStatusCode());
+    
+            foreach ($response->getHeaders() as $name => $values) {
+                foreach ($values as $value) {
+                    header("$name: $value", false);
+                }
+            }
+        }
+    
+        $stream = $response->getBody();
+    
+        if ($stream->isSeekable()) {
+            $stream->rewind();
+        }
+    
+        while (!$stream->eof()) {
+            echo $stream->read(1024 * 8);
+        }
+    
+        if (\function_exists('fastcgi_finish_request')) {
+            fastcgi_finish_request();
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use function Http\Response\send;
+use PgFramework\Response\ResponseSender;
 
 $basePath = dirname(__DIR__);
 
@@ -11,5 +11,5 @@ $app = require $basePath . '/app/Bootstrap/Bootstrap.php';
 
 if (php_sapi_name() !== 'cli') {
     $response = $app->run();
-    send($response);
+    ResponseSender::send($response);
 }
