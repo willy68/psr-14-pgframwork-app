ea9c7fb316083ede3a468445c8a1fc10f6b93d0e
William Lety <william.lety@gmail.com>
2022-04-21 16:49:40 +0200

    Prepare using Mezzio Session

---
+++ PgFramework/Session/Listener/SessionListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PgFramework\Session\Listener;
+
+use PgFramework\Event\Events;
+use League\Event\ListenerPriority;
+use Mezzio\Session\SessionInterface;
+use PgFramework\Event\RequestEvent;
+use PgFramework\Event\ResponseEvent;
+use Mezzio\Session\SessionPersistenceInterface;
+use PgFramework\EventDispatcher\EventSubscriberInterface;
+
+class SessionListener implements EventSubscriberInterface
+{
+    public const SESSION_ATTRIBUTE = '_session';
+
+    private $persistence;
+
+    private $session;
+
+    public function __construct(SessionPersistenceInterface $persistence, SessionInterface $session)
+    {
+        $this->persistence = $persistence;
+        $this->session = $session;
+    }
+
+    public function onRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        $event->setRequest(
+            $request
+                ->withAttribute(self::SESSION_ATTRIBUTE, $this->session)
+                ->withAttribute(SessionInterface::class, $this->session)
+        );
+    }
+
+    public function onResponse(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $event->setResponse($this->persistence->persistSession($this->session, $response));
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            Events::REQUEST => ['onRequest', ListenerPriority::HIGH],
+            Events::RESPONSE => ['onResponse', ListenerPriority::LOW],
+        ];
+    }
+}
--- app/Bootstrap/Bootstrap.php
+++ app/Bootstrap/Bootstrap.php
@@ -2,34 +2,37 @@
 
 declare(strict_types=1);
 
+use PgFramework\App;
 use PgFramework\Environnement\Environnement;
 use PgFramework\File\FileUtils;
 use Symfony\Component\Dotenv\Dotenv;
 
-if (!class_exists(Dotenv::class)) {
-    throw new Exception("le library symfony/dotenv est pas installée, lancez composer symfony/dotenv!");
-}
+return (static function (): App {
+    if (!class_exists(Dotenv::class)) {
+        throw new Exception("le library symfony/dotenv est pas installée, lancez composer symfony/dotenv!");
+    }
 
-if (!isset($basePath)) {
-    $basePath = dirname(__DIR__, 2);
-}
+    if (!isset($basePath)) {
+        $basePath = dirname(__DIR__, 2);
+    }
 
-$dotenv = new Dotenv();
-$dotenv->bootEnv($basePath . '/.env');
+    $dotenv = new Dotenv();
+    $dotenv->bootEnv($basePath . '/.env');
 
-$bootstrap = require 'App.php';
+    $bootstrap = require 'App.php';
 
-$config = FileUtils::getFiles($basePath . '/config', 'php', '.dist.');
+    $config = FileUtils::getFiles($basePath . '/config', 'php', '.dist.');
 
-$app = (new PgFramework\App(array_keys($config)))
-    ->addModules($bootstrap['modules'])
-    //->addMiddlewares($bootstrap['middlewares']);
-    ->addListeners($bootstrap['listeners']);
+    $app = (new App(array_keys($config)))
+        ->addModules($bootstrap['modules'])
+        //->addMiddlewares($bootstrap['middlewares']);
+        ->addListeners($bootstrap['listeners']);
 
-if (Environnement::getEnv('APP_ENV', 'prod') === 'dev') {
-    $whoops = new \Whoops\Run();
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
-    $whoops->register();
-}
+    if (Environnement::getEnv('APP_ENV', 'prod') === 'dev') {
+        $whoops = new \Whoops\Run();
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+        $whoops->register();
+    }
 
-return $app;
+    return $app;
+})();
