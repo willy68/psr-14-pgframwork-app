566559b75783f95764c1625b2b351e32cb6b56f6
William Lety <william.lety@gmail.com>
2022-04-21 14:11:31 +0200

    Use own package response-sender

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use PgFramework\Response\ResponseSender;
+use function Http\Response\send;
 
 $basePath = dirname(__DIR__);
 
@@ -11,5 +11,5 @@ $app = require $basePath . '/app/Bootstrap/Bootstrap.php';
 
 if (php_sapi_name() !== 'cli') {
     $response = $app->run();
-    ResponseSender::send($response);
+    send($response);
 }
