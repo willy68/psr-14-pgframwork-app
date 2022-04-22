d53dd2dfe98040d4160d8c3f82e7b287f148cfe7
William Lety <william.lety@gmail.com>
2022-04-22 16:36:38 +0200

    Phpcbf typo

--- PgFramework/Response/ResponseSender.php
+++ PgFramework/Response/ResponseSender.php
@@ -21,26 +21,26 @@ class ResponseSender
                 $response->getStatusCode(),
                 $response->getReasonPhrase()
             );
-    
+
             header($http_line, true, $response->getStatusCode());
-    
+
             foreach ($response->getHeaders() as $name => $values) {
                 foreach ($values as $value) {
                     header("$name: $value", false);
                 }
             }
         }
-    
+
         $stream = $response->getBody();
-    
+
         if ($stream->isSeekable()) {
             $stream->rewind();
         }
-    
+
         while (!$stream->eof()) {
             echo $stream->read(1024 * 8);
         }
-    
+
         if (\function_exists('fastcgi_finish_request')) {
             fastcgi_finish_request();
         }
