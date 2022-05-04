fcf3bbb88f3dbffed97b42f5cf4187d4030fb2f1
William Lety <william.lety@gmail.com>
2022-05-04 17:57:31 +0200

    Help Intellisense

--- PgFramework/Validator/Rules/ExistsValidation.php
+++ PgFramework/Validator/Rules/ExistsValidation.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PgFramework\Validator\Rules;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use PgFramework\Validator\ValidationInterface;
 
