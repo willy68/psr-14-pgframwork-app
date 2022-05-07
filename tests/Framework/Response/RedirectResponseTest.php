a4af8a0b8cd5cbfad138cc8469cf843e15447c9c
William Lety <william.lety@gmail.com>
2022-05-08 00:05:37 +0200

    RedirectResponseTest

--- tests/Framework/Response/RedirectResponseTest.php
+++ tests/Framework/Response/RedirectResponseTest.php
@@ -1,21 +1,21 @@
 <?php
+
 namespace Tests\Framework\Response;
 
-use Framework\Response\RedirectResponse;
+use PgFramework\Response\ResponseRedirect;
 use PHPUnit\Framework\TestCase;
 
 class RedirectResponseTest extends TestCase
 {
-
     public function testStatus()
     {
-        $response = new RedirectResponse('/demo');
+        $response = new ResponseRedirect('/demo');
         $this->assertEquals(301, $response->getStatusCode());
     }
 
     public function testHeader()
     {
-        $response = new RedirectResponse('/demo');
+        $response = new ResponseRedirect('/demo');
         $this->assertEquals(['/demo'], $response->getHeader('Location'));
     }
 }
