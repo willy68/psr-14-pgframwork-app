fbe833e49555d3bf58291f95bf27f3209e6fb72f
William Lety <william.lety@gmail.com>
2022-05-08 00:26:09 +0200

    FlashServiceTest

--- PgFramework/Session/ArraySession.php
+++ PgFramework/Session/ArraySession.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace PgFramework\Session;
 
+use Mezzio\Session\SessionInterface;
+
 class ArraySession implements SessionInterface
 {
     private $session = [];
+
+    private $changed = false;
+
+    private $regenerated = false;
 
     /**
      * @param string $key
@@ -29,14 +35,50 @@ class ArraySession implements SessionInterface
     public function set(string $key, $value): void
     {
         $this->session[$key] = $value;
+        $this->changed = true;
     }
 
     /**
      * @param string $key
      * @return void
      */
-    public function delete(string $key): void
+    public function unset(string $key): void
     {
         unset($this->session[$key]);
+        $this->changed = true;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->session);
+    }
+
+    public function clear(): void
+    {
+        $this->session = [];
+        $this->changed = true;
+    }
+
+    public function toArray(): array
+    {
+        return $this->session;
+    }
+
+    public function hasChanged(): bool
+    {
+        return $this->changed;
+    }
+
+    public function regenerate(): SessionInterface
+    {
+        $session = new self();
+        $session->session = $this->session;
+        $this->regenerated = true;
+        return $session;
+    }
+
+    public function isRegenerated(): bool
+    {
+        return $this->regenerated;
     }
 }
--- tests/Framework/Session/FlashServiceTest.php
+++ tests/Framework/Session/FlashServiceTest.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace Tests\Framework\Session;
 
-use Framework\Session\ArraySession;
-use Framework\Session\FlashService;
+use PgFramework\Session\ArraySession;
+use PgFramework\Session\FlashService;
 use PHPUnit\Framework\TestCase;
 
 class FlashServiceTest extends TestCase
 {
-
     /**
      * @var ArraySession
      */
