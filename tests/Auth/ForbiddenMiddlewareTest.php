0c306b95d96ea10be19507822ca5d5885fc1801c
William Lety <william.lety@gmail.com>
2022-05-10 00:25:02 +0200

    ForbidenMiddlewareTest

--- tests/Auth/ForbiddenMiddlewareTest.php
+++ tests/Auth/ForbiddenMiddlewareTest.php
@@ -13,6 +13,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use TypeError;
 
 class ForbiddenMiddlewareTest extends TestCase
 {
@@ -32,7 +33,6 @@ class ForbiddenMiddlewareTest extends TestCase
         $uri->method('getPath')->willReturn($path);
         $request = $this->getMockBuilder(ServerRequestInterface::class)->getMock();
         $request->method('getUri')->willReturn($uri);
-        $request->method('getHeaderLine')->willReturn('');
         return $request;
     }
 
@@ -53,6 +53,7 @@ class ForbiddenMiddlewareTest extends TestCase
     {
         $delegate = $this->makeDelegate();
         $request = $this->makeRequest('/test');
+        $request->method('getHeaderLine')->willReturn('');
         $delegate->expects($this->once())->method('handle')->willThrowException(new ForbiddenException());
         /** @var ServerRequestInterface $request */
         /** @var RequestHandlerInterface $delegate*/
@@ -62,29 +63,51 @@ class ForbiddenMiddlewareTest extends TestCase
         $this->assertEquals('/test', $this->session->get('auth.redirect'));
     }
 
+    public function testCatchForbiddenExceptionJson()
+    {
+        $delegate = $this->makeDelegate();
+        $request = $this->makeRequest('/test');
+        $request->method('getHeaderLine')->willReturn('application/json');
+        $delegate->expects($this->once())->method('handle')->willThrowException(new ForbiddenException());
+        /** @var ServerRequestInterface $request */
+        /** @var RequestHandlerInterface $delegate*/
+        $response = $this->makeMiddleware()->process($request, $delegate);
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
     public function testCatchFailedAccessException()
     {
         $delegate = $this->makeDelegate();
         $request = $this->makeRequest('/test');
+        $request->method('getHeaderLine')->willReturn('');
+        $request->method('getServerParams')->willReturn(['HTTP_REFERER' => '/test']);
         $delegate->expects($this->once())->method('handle')->willThrowException(new FailedAccessException());
         /** @var ServerRequestInterface $request */
         /** @var RequestHandlerInterface $delegate*/
         $response = $this->makeMiddleware()->process($request, $delegate);
         $this->assertEquals(301, $response->getStatusCode());
-        $this->assertEquals(
-            [
-                isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/login'
-            ],
-            $response->getHeader('Location')
-        );
+        $this->assertEquals(['/test'], $response->getHeader('Location'));
+    }
+
+    public function testCatchFailedAccessExceptionJson()
+    {
+        $delegate = $this->makeDelegate();
+        $request = $this->makeRequest('/test');
+        $request->method('getHeaderLine')->willReturn('application/json');
+        $delegate->expects($this->once())->method('handle')->willThrowException(new FailedAccessException());
+        /** @var ServerRequestInterface $request */
+        /** @var RequestHandlerInterface $delegate*/
+        $response = $this->makeMiddleware()->process($request, $delegate);
+        $this->assertEquals(403, $response->getStatusCode());
     }
 
     public function testCatchTypeErrorException()
     {
         $delegate = $this->makeDelegate();
         $request = $this->makeRequest('/test');
-        $delegate->expects($this->once())->method('handle')->willReturnCallback(function (UserInterface $user) {
-            return true;
+        $request->method('getHeaderLine')->willReturn('');
+        $delegate->expects($this->once())->method('handle')->willReturnCallback(function () {
+            throw new \TypeError(UserInterface::class, 200);
         });
         /** @var ServerRequestInterface $request */
         /** @var RequestHandlerInterface $delegate*/
@@ -98,6 +121,7 @@ class ForbiddenMiddlewareTest extends TestCase
     {
         $delegate = $this->makeDelegate();
         $request = $this->makeRequest('/test');
+        $request->method('getHeaderLine')->willReturn('');
         $delegate->expects($this->once())->method('handle')->willReturnCallback(function () {
             throw new \TypeError("test", 200);
         });
@@ -111,10 +135,30 @@ class ForbiddenMiddlewareTest extends TestCase
         }
     }
 
+    public function testTypeErrorExceptionJson()
+    {
+        $delegate = $this->makeDelegate();
+        $request = $this->makeRequest('/test');
+        $request->method('getHeaderLine')->willReturn('application/json');
+        $delegate->expects($this->once())->method('handle')->willReturnCallback(function () {
+            throw new \TypeError(UserInterface::class, 200);
+        });
+        try {
+            /** @var ServerRequestInterface $request */
+            /** @var RequestHandlerInterface $delegate*/
+            $response = $this->makeMiddleware()->process($request, $delegate);
+        } catch (\TypeError $e) {
+            $this->assertEquals("test", $e->getMessage());
+            $this->assertEquals(200, $e->getCode());
+        }
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
     public function testProcessValidRequest()
     {
         $delegate = $this->makeDelegate();
         $request = $this->makeRequest('/test');
+        $request->method('getHeaderLine')->willReturn('');
         $response = $this->getMockBuilder(ResponseInterface::class)->getMock();
         $delegate
             ->expects($this->once())
