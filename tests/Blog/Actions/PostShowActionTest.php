16c98ad3d73791bfe69022b79af49d262328cffb
William Lety <william.lety@gmail.com>
2022-05-09 10:26:27 +0200

    PostShowActionTest

--- tests/Blog/Actions/PostShowActionTest.php
+++ tests/Blog/Actions/PostShowActionTest.php
@@ -2,14 +2,13 @@
 
 namespace Tests\App\Blog\Actions;
 
-use Framework\Router;
-use App\Blog\Entity\Post;
-use App\Blog\Table\PostTable;
+use App\Entity\Post;
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Psr7\ServerRequest;
+use Mezzio\Router\RouterInterface;
 use Prophecy\PhpUnit\ProphecyTrait;
 use App\Blog\Actions\PostShowAction;
-use Framework\Renderer\RendererInterface;
+use App\Repository\PostRepository;
+use PgFramework\Renderer\RendererInterface;
 
 class PostShowActionTest extends TestCase
 {
@@ -22,20 +21,18 @@ class PostShowActionTest extends TestCase
 
     private $renderer;
 
-    private $postTable;
+    private $postRepository;
 
     private $router;
 
     public function setUp(): void
     {
         $this->renderer = $this->prophesize(RendererInterface::class);
-        $this->postTable = $this->prophesize(PostTable::class);
-        // PDO
-        $this->router = $this->prophesize(Router::class);
+        $this->postRepository = $this->prophesize(PostRepository::class);
+        $this->router = $this->prophesize(RouterInterface::class);
         $this->action = new PostShowAction(
             $this->renderer->reveal(),
-            $this->router->reveal(),
-            $this->postTable->reveal()
+            $this->router->reveal()
         );
     }
 
@@ -43,25 +40,25 @@ class PostShowActionTest extends TestCase
     {
         // Article
         $post = new Post();
-        $post->id = $id;
-        $post->slug = $slug;
+        $post->setId($id)
+            ->setSlug($slug);
         return $post;
     }
 
     public function testShowRedirect()
     {
         $post = $this->makePost(9, "azezae-azeazae");
-        $request = (new ServerRequest('GET', '/'))
-            ->withAttribute('id', $post->id)
-            ->withAttribute('slug', 'demo');
 
         $this->router->generateUri(
             'blog.show',
-            ['id' => $post->id, 'slug' => $post->slug]
+            ['id' => $post->getId(), 'slug' => $post->getSlug()]
         )->willReturn('/demo2');
-        $this->postTable->findWithCategory($post->id)->willReturn($post);
+        $this->postRepository->findWithCategory($post->getId())->willReturn($post);
+        $this->renderer->render('@blog/show', ['post' => $post])->willReturn('');
 
-        $response = call_user_func_array($this->action, [$request]);
+        /** @var callable $callable */
+        $callable = $this->action;
+        $response = call_user_func_array($callable, ['slug', $post]);
         $this->assertEquals(301, $response->getStatusCode());
         $this->assertEquals(['/demo2'], $response->getHeader('location'));
     }
@@ -69,13 +66,12 @@ class PostShowActionTest extends TestCase
     public function testShowRender()
     {
         $post = $this->makePost(9, "azezae-azeazae");
-        $request = (new ServerRequest('GET', '/'))
-            ->withAttribute('id', $post->id)
-            ->withAttribute('slug', $post->slug);
-        $this->postTable->findWithCategory($post->id)->willReturn($post);
-        $this->renderer->render('@blog/show', ['post' => $post])->willReturn('');
+        $this->postRepository->findWithCategory($post->getId())->willReturn($post);
+        $this->renderer->render('@blog/show', ['post' => $post])->willReturn($post->getSlug());
 
-        $response = call_user_func_array($this->action, [$request]);
-        $this->assertEquals(true, true);
+        /** @var callable $callable */
+        $callable = $this->action;
+        $response = call_user_func_array($callable, ['azezae-azeazae', $post]);
+        $this->assertEquals($post->getSlug(), $response);
     }
 }
