d4b02f84f45324f0f97083f6ff602fcd9bae106f
William Lety <william.lety@gmail.com>
2022-05-10 10:48:40 +0200

    CookieLogin and Logout Test

--- tests/Framework/Auth/AuthorizationMiddlewareTest.php
+++ tests/Framework/Auth/AuthorizationMiddlewareTest.php
@@ -15,20 +15,23 @@ class AuthorizationMiddlewareTest extends TestCase
 {
     private $voterManager;
 
+    private $auth;
+
+    private $accesMap;
+
     public function setUp(): void
     {
         $this->voterManager = $this->getMockBuilder(VoterManager::class)->getMock();
+        $this->auth = $this->getMockBuilder(Auth::class)->getMock();
+        $this->accesMap = $this->getMockBuilder(AccessMapInterface::class)->getMock();
     }
 
-    public function makeMiddleware($user)
+    public function makeMiddleware($user, $attributes = [['ROLE_ADMIN']], $vote = true)
     {
-        $auth = $this->getMockBuilder(Auth::class)->getMock();
-        $auth->method('getUser')->willReturn($user);
-        $accesMap = $this->getMockBuilder(AccessMapInterface::class)->getMock();
-        $accesMap->method('getPatterns')->willReturn([['ROLE_ADMIN']]);
-        /** @var Auth $auth */
-        /** @var AccessMapInterface $accesMap */
-        return new AuthorizationMiddleware($auth, $this->voterManager, $accesMap);
+        $this->auth->method('getUser')->willReturn($user);
+        $this->accesMap->method('getPatterns')->willReturn($attributes);
+        $this->voterManager->method('decide')->willReturn($vote);
+        return new AuthorizationMiddleware($this->auth, $this->voterManager, $this->accesMap);
     }
 
     public function makeDelegate($calls)
@@ -38,6 +41,15 @@ class AuthorizationMiddlewareTest extends TestCase
         $delegate->expects($calls)->method('handle')->willReturn($response);
         /** @var RequestHandlerInterface $delegate */
         return $delegate;
+    }
+
+    public function testNextIfNoAttributes()
+    {
+        $request = (new ServerRequest('GET', '/demo/'));
+        $this->makeMiddleware(null, [[]])->process(
+            $request,
+            $this->makeDelegate($this->once())
+        );
     }
 
     public function testThrowIfNoUser()
@@ -64,10 +76,9 @@ class AuthorizationMiddlewareTest extends TestCase
     public function testThrowIfNoAdmin()
     {
         $user = $this->getMockBuilder(Auth\UserInterface::class)->getMock();
-        $this->voterManager->method('decide')->willReturn(false);
         $request = (new ServerRequest('GET', '/demo/'));
         $this->expectException(Auth\FailedAccessException::class);
-        $this->makeMiddleware($user)->process(
+        $this->makeMiddleware($user, [['ROLE_ADMIN']], false)->process(
             $request,
             $this->makeDelegate($this->never())
         );
