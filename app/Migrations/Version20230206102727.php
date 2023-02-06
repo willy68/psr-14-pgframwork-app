f07fba1bbb86e797f7f8859088a53ecfc91e2531
William Lety <william.lety@gmail.com>
2023-02-06 11:32:15 +0100

    Add user reset password migration

---
+++ app/Migrations/Version20230206102727.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230206102727 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users ADD password_reset VARCHAR(255) DEFAULT NULL, ADD password_reset_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users DROP password_reset, DROP password_reset_at');
+    }
+}
