5e9d59423f17092974714f6c6eeb6cf9a5c3718a
William Lety <william.lety@gmail.com>
2023-02-28 22:28:46 +0100

    PHP 8.1

--- app/Demo/Controller/DemoController.php
+++ app/Demo/Controller/DemoController.php
@@ -9,22 +9,20 @@ use App\Entity\Post;
 use App\Models\Client;
 use App\Auth\Models\User;
 use App\Repository\PostRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Psr\Container\ContainerInterface;
+use PDO;
 use PgFramework\Router\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use PgFramework\Renderer\RendererInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use PgFramework\Invoker\Annotation\ParameterConverter;
 use PgFramework\Database\ActiveRecord\ActiveRecordQuery;
 
 class DemoController
 {
     /**
-     * Montre l'index de l'application
+     * Montre l’index de l’application
      * $renderer est injecté automatiquement, comme toutes les classes
      * renseignées dans config/config.php
-     * Il est possible d'injecter la ServerRequestInterface
+     * Il est possible d’injecter la ServerRequestInterface
      * et les paramètres de la route (ex. $id).
      * Ce type d'injection est possible avec \DI\Container de PHP-DI
      *
@@ -32,39 +30,33 @@ class DemoController
      *
      * @param ServerRequestInterface $request
      * @param RendererInterface $renderer
-     * @param \PDO $pdo
-     * @param ContainerInterface $c
+     * @param PDO $pdo
+     * @param ManagerRegistry $managerRegistry
      * @return string
      */
     #[Route('/', name: 'demo.index', methods: ['GET'])]
     public function index(
         ServerRequestInterface $request,
         RendererInterface $renderer,
-        \PDO $pdo,
-        EntityManagerInterface $em,
-        ContainerInterface $c,
+        PDO $pdo,
         ManagerRegistry $managerRegistry
     ): string {
         $conn = $managerRegistry->getManager();
 
-        /** @var PostRepository */
+        /** @var PostRepository $rp*/
         $rp = $conn->getRepository(Post::class);
-        $pc = $rp->findWithCategory(122);
+        $rp->findWithCategory(122);
 
         $query = new ActiveRecordQuery();
         $query
             ->where('id = ?', 'user_id = ?')
             ->orWhere('created_at = now()')
             ->setWhereValue([2, 5, new DateTime()]);
-        /** @var \App\Auth\Models\User $user */
+        /** @var User $user */
         $user = User::find_by_username(['username' => 'admin']);
         $user_array = $user->to_array();
 
-        /** @var PostRepository */
-        $repo = $em->getRepository(Post::class);
-        $doctrinePost = $repo->findWithCategory(122);
-
-        $mysql_ver = $pdo->getAttribute(\PDO::ATTR_SERVER_VERSION);
+        $mysql_ver = $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
         $params = array_merge($request->getServerParams(), $user_array, [$mysql_ver], [$query]);
         return $renderer->render('@demo/index', compact('params'));
     }
@@ -86,8 +78,8 @@ class DemoController
      *
      * @ParameterConverter("client", options={"id"="id", "include"="adresses"})
      *
-     * @param \App\Models\Client $client
-     * @param \PgFramework\Renderer\RendererInterface $renderer
+     * @param Client $client
+     * @param RendererInterface $renderer
      * @return string
      */
     #[Route('/demo/client/{id:\d+}', name: 'demo.client', methods: ['GET'])]
