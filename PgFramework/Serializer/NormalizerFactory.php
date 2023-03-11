a622da1b6f011828edb67b9a4f57a121ba7c94ea
William Lety <william.lety@gmail.com>
2023-03-11 15:27:23 +0100

    Add normalizer

--- PgFramework/Serializer/NormalizerFactory.php
+++ PgFramework/Serializer/NormalizerFactory.php
@@ -4,20 +4,60 @@ declare(strict_types=1);
 
 namespace PgFramework\Serializer;
 
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
+use Symfony\Component\Serializer\Normalizer\ConstraintViolationListNormalizer;
+use Symfony\Component\Serializer\Normalizer\DataUriNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateIntervalNormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeZoneNormalizer;
+use Symfony\Component\Serializer\Normalizer\FormErrorNormalizer;
+use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
+use Symfony\Component\Serializer\Normalizer\MimeMessageNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\ProblemNormalizer;
+use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
+use Symfony\Component\Serializer\Normalizer\UidNormalizer;
+use Symfony\Component\Serializer\Normalizer\UnwrappingDenormalizer;
 
 class NormalizerFactory
 {
-    public function __invoke(): array
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __invoke(ContainerInterface $c): array
     {
+
+        $debug = $c->get('env') !== 'prod';
         $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader());
-        return [
+        $normalizers =  [
+            new UnwrappingDenormalizer(),
+            new ProblemNormalizer(debug: $debug),
+            new UidNormalizer(),
+            new JsonSerializableNormalizer(),
             new DateTimeNormalizer(),
-            new ObjectNormalizer($classMetadataFactory),
+            new ConstraintViolationListNormalizer(),
+            new MimeMessageNormalizer(new PropertyNormalizer()),
+            new DateTimeZoneNormalizer(),
+            new DateIntervalNormalizer(),
+            new FormErrorNormalizer(),
+            new BackedEnumNormalizer(),
+            new DataUriNormalizer(),
+            new ArrayDenormalizer(),
+            new ObjectNormalizer($classMetadataFactory)
         ];
+
+        // Normalizer from symfony/messenger, if exists
+        /*if (\class_exists(FlattenExceptionNormalizer::class)) {
+            $normalizers[] = new FlattenExceptionNormalizer();
+        }*/
+        return $normalizers;
     }
 
 }
