bd2fc8fad090095e2908113f706f3a391fcf0de0
William Lety <william.lety@gmail.com>
2023-03-12 11:39:13 +0100

    Add trow block PHPDoc

--- PgFramework/Session/SessionFactory.php
+++ PgFramework/Session/SessionFactory.php
@@ -8,10 +8,16 @@ use Mezzio\Session\LazySession;
 use Mezzio\Session\SessionInterface;
 use Mezzio\Session\SessionPersistenceInterface;
 use PgFramework\ApplicationInterface;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class SessionFactory
 {
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function __invoke(ContainerInterface $c): SessionInterface
     {
         $persistence = $c->get(SessionPersistenceInterface::class);
