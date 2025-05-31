70c5fcda8521ae2c579af6d3798b510869ff431a
willy <william.lety@gmail.com>
2025-06-01 00:06:46 +0200

    First commit

--- PgFramework/Security/Firewall/FirewallEvents.php
+++ PgFramework/Security/Firewall/FirewallEvents.php
@@ -8,7 +8,7 @@ class FirewallEvents
 {
     public const AUTHENTICATION = 'firewall.event.authentication';
     public const AUTHORIZATION = 'firewall.event.authorization';
-    public const SECUTITY = 'firewall.event.security';
+    public const SECURITY = 'firewall.event.security';
     public const LOGIN_SUCCESS = 'firewall.event.login.success';
     public const LOGIN_FAILED = 'firewall.event.login.failed';
 }
